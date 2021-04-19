cc9b455c6e1fd79cec67c7f05fee7f88530f650e
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-19 09:43:25 +0800

    Lists and Passing State with Props and Functional Components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 
-class Navbar extends Component {
-    // Set default props
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    }
-    // Check prop types
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
-    render() {
+const Navbar = ({icon, title}) => {
         return (
             <nav className="navbar bg-primary">
                 <h1>
-                    <i className={this.props.icon}/>
-                    {this.props.title}
+                    <i className={icon}/>
+                    {title}
                 </h1>
             </nav>
         )
-    }
+}
+
+
+// Set default props
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+}
+// Check prop types
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
 }
 
 export default Navbar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,14 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export class UserItem extends Component {
-    state = {
-        "login": "mojombo",
-        "id": 1,
-        "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
-        "html_url": "https://github.com/mojombo"
-    }
-    render() {
-        const {login, avatar_url, html_url} = this.state;
+//  receiving the props being passed down from Users.js
+const UserItem = ({userData: {login, avatar_url, html_url}}) => {
         return (
             <div className="card text-center">
                 <img 
@@ -21,8 +15,11 @@ export class UserItem extends Component {
                     <a href={html_url} className="btn btn-dark btn-sm my-1">View</a>
                 </div>
             </div>
-        )
-    }
+        )    
+}
+
+UserItem.propTypes = {
+    userData: PropTypes.object.isRequired,
 }
 
 export default UserItem
