03cb9671648275ee7efb172b87c530f370375fa2
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-21 10:33:36 +0800

    passing props up & search and clear users form state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,63 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar'
+import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
-
 class App extends Component {
-  state = {
-    users: [],
-    loading: false
-  }
-  // Lifecycle method: it fires on load
-  async componentDidMount() {
+	state = {
+		users: [],
+		loading: false
+	};
+	// Lifecycle method: it fires on load
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
 
-    this.setState({loading: true});
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 	);
+	// 	this.setState({ users: res.data, loading: false });
+	// }
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({users: res.data, loading: false});
-      
-  }
-  // Lifecycle method
+	// Search Github users: receiving the props being passed up from users
+	searchUsers = async (text) => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ users: res.data.items, loading: false });
+	};
+	// clear users from state
+	clearUsers = () => this.setState({ users: [], loading: false });
+
+	// Lifecycle method
 	render() {
+		const { users, loading } = this.state;
 		return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users}/>
-        </div>
-      </div>
+			<div className='App'>
+				<Navbar />
+				<div className='container'>
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+					/>
+					<Users loading={loading} users={users} />
+				</div>
+			</div>
 		);
 
-    // JSX and expression
+		// JSX and expression
 
-    //   // JSX are rendered here (underneath is JS)
-    //   <div className='App'>
-    //   {/* must be wrapped with one parent element */}
-    //   {loading ? <h4>Loading...</h4> : 		    <h1>Hello {showName && name}</h1>
-    //   }
-    // </div>
+		//   // JSX are rendered here (underneath is JS)
+		//   <div className='App'>
+		//   {/* must be wrapped with one parent element */}
+		//   {loading ? <h4>Loading...</h4> : 		    <h1>Hello {showName && name}</h1>
+		//   }
+		// </div>
 
-    // Pure JS way
+		// Pure JS way
 		// return React.createElement(
 		// 	'div',
 		// 	{ className: 'App' },
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
 	// Component level state
 	state = {
 		text: ''
 	};
+	// making the code more robust
+	static propTypes = {
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired
+	};
+
 	onChange = (e) => {
 		// use [e.target.name] can allow you to share the onchange with different input fields
 		this.setState({ [e.target.name]: e.target.value });
 	};
 	onSubmit = (e) => {
 		e.preventDefault();
-		console.log(this.state.text);
+		// Passing the props up
+		this.props.searchUsers(this.state.text);
+		// Clear the input field state
+		this.setState({ text: '' });
 	};
 
 	render() {
+		const { showClear, clearUsers } = this.props;
 		return (
 			<div>
 				<form onSubmit={this.onSubmit} className='form'>
@@ -32,6 +44,14 @@ export class Search extends Component {
 						className='btn btn-dark btn-block'
 					/>
 				</form>
+				{showClear && (
+					<button
+						className='btn btn-light btn-block'
+						onClick={clearUsers}
+					>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
