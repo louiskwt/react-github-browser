cdf2dc1645db5318ce56b35fc917e5895e5555bb
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-22 10:08:05 +0800

    React Router Setup

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-
-const Navbar = ({icon, title}) => {
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    <i className={icon}/>
-                    {title}
-                </h1>
-            </nav>
-        )
-}
-
+const Navbar = ({ icon, title }) => {
+	return (
+		<nav className='navbar bg-primary'>
+			<h1>
+				<i className={icon} />
+				{title}
+			</h1>
+			<ul>
+				<Link to='/'>Home</Link>
+				<Link to='/about'>About</Link>
+			</ul>
+		</nav>
+	);
+};
 
 // Set default props
 Navbar.defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github'
-}
+	title: 'Github Finder',
+	icon: 'fab fa-github'
+};
 // Check prop types
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-}
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired
+};
 
-export default Navbar
+export default Navbar;
