2041adc86dcbaa6145054ff0d4db9082f120c13b
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-25 10:32:38 +0800

    User, Repos, and Alert Context

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,9 +1,10 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ showClear, clearUsers, setAlert }) => {
+const Search = () => {
 	const githubContext = useContext(GithubContext);
+	const alertContext = useContext(AlertContext);
 
 	// Component level state
 	const [text, setText] = useState('');
@@ -14,7 +15,7 @@ const Search = ({ showClear, clearUsers, setAlert }) => {
 	const onSubmit = (e) => {
 		e.preventDefault();
 		if (text === '') {
-			setAlert('Please enter something', 'light');
+			alertContext.setAlert('Please enter something', 'light');
 		} else {
 			// Passing the props up
 			githubContext.searchUsers(text);
@@ -40,22 +41,16 @@ const Search = ({ showClear, clearUsers, setAlert }) => {
 					className='btn btn-dark btn-block'
 				/>
 			</form>
-			{showClear && (
+			{githubContext.users.length > 0 && (
 				<button
 					className='btn btn-light btn-block'
-					onClick={clearUsers}
+					onClick={githubContext.clearUsers}
 				>
 					Clear
 				</button>
 			)}
 		</div>
 	);
-};
-
-Search.propTypes = {
-	clearUsers: PropTypes.func.isRequired,
-	showClear: PropTypes.bool.isRequired,
-	setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import UserItem from '../users/UserItem'
+import React, { useContext } from 'react';
+import UserItem from '../users/UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types'
+import GithubContext from '../../context/github/githubContext';
 
+const Users = () => {
+	const githubContext = useContext(GithubContext);
 
-const Users = ({users, loading}) => {
-    if(loading) {
-        return <Spinner />
-    } else {
-        return (
-            <div style={userStyle}>
-                {/* Loop through the user data to create a list ( ) */}
-                {users.map(user => (
-                    <UserItem key={user.id} userData={user} />
-                ) )}
-                    
-            </div>
-        )
-    }
-}
+	const { loading, users } = githubContext;
 
-Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
-}
+	if (loading) {
+		return <Spinner />;
+	} else {
+		return (
+			<div style={userStyle}>
+				{/* Loop through the user data to create a list ( ) */}
+				{users.map((user) => (
+					<UserItem key={user.id} userData={user} />
+				))}
+			</div>
+		);
+	}
+};
 
 const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
-}
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	gridGap: '1rem'
+};
 
-export default Users
+export default Users;
